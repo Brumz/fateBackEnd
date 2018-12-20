0aca349e447d5ba76dd9a7f08827142184977eb5
debroniswarren <debronisw@gmail.com>
2018-12-20 14:05:45 -0700

    Added images to each profile

--- migrations/20181217162215_profiles.js
+++ migrations/20181217162215_profiles.js
@@ -3,6 +3,7 @@ exports.up = function (knex, Promise) {
         profile.increments("id")
         profile.string("first_name").notNullable()
         profile.string("last_name").notNullable()
+        profile.string("url").notNullable()
         profile.integer("accountUser_id").references("id").inTable("accountUsers").onDelete("CASCADE")
         profile.integer("rating_id").references("id").inTable("ratings").onDelete("CASCADE")
     })
